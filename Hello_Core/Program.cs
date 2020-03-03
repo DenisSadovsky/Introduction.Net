4f120ce89e62bdf3c142f84668fea46ac15e998f
Dzianis Sadouski <Dzianis_Sadouski@epam.com>
2020-03-03 16:32:18 +0300

    Added .NET Core app

---
+++ Hello_Core/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hello_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hello {name}");
+
+            Console.ReadKey();
+            
+        }
+    }
+}
